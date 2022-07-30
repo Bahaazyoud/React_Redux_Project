6e9c2aa11747f6828cff6a764e6221274a432d13
Bahaazyoud <bahaazyoud2000@gmail.com>
2022-07-30 18:10:34 +0300

    upload

--- redux_project/src/App.js
+++ redux_project/src/App.js
@@ -1,13 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import {Login} from "./pages/login";
+import Registeration from "./pages/registeration";
 import Dashboard from "./admin/Dashboard";
- 
- 
-
 
 function App() {
   return (
-    <div >
-      <Dashboard/>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/register" element={<Registeration />} />
+      </Routes>
+    </Router>
   );
 }
 

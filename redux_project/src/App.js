92d89c1d706010298b47208f4d2e80fcf4b3ad3b
Bahaazyoud <bahaazyoud2000@gmail.com>
2022-07-30 18:27:51 +0300

    upload

--- redux_project/src/App.js
+++ redux_project/src/App.js
@@ -1,9 +1,10 @@
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import {Login} from "./pages/login";
 import Registeration from "./pages/registeration";
 import Dashboard from "./admin/Dashboard";
+import ShowUsers from "./admin/showUsers";
+ 
 
 function App() {
   return (
@@ -13,38 +14,12 @@ function App() {
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/login" element={<Login/>} />
         <Route exact path="/register" element={<Registeration />} />
+        <Route exact path="/Users" element={<ShowUsers/>} />
       </Routes>
     </Router>
-=======
- 
-import Dashboard from "./admin/Dashboard";
-import ShowUsers from "./admin/showUsers";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
- 
- 
-
-
-function App() {
-  return (
-    <div >
-    
-    <Router>
-
-
-      <Routes>
-      <Route path="/" element={<Dashboard/>} />
-
-        <Route path="/Users" element={<ShowUsers/>} />
-        
-
-      </Routes>
-
-    </Router>
-
-   
-    </div>
->>>>>>> e44e198930f2b32fe37403590ae740dbb7eb8668
-  );
+  )
 }
+ 
+ 
 
 export default App;

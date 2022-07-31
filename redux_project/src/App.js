4adc6edb4144784f64a21195a35ba8f86ec795da
shifaalkasasbeh <100359607+shifaalkasasbeh@users.noreply.github.com>
2022-07-31 15:04:05 +0300

    newwork

--- redux_project/src/App.js
+++ redux_project/src/App.js
@@ -1,58 +1,32 @@
-<<<<<<< HEAD
- 
-import Dashboard from "./admin/Dashboard";
-import ShowUsers from "./admin/showUsers";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
- import Register  from "./pages/Login/Register";
-  import  Login  from "./pages/Login/Login";
-
-function App() {
-  return (
-    <div >
-    
-    <Router>
-
-
-      <Routes>
-      <Route path="/" element={<Dashboard/>} />
-
-        <Route path="/Users" element={<ShowUsers/>} />
-        
-        <Route path="/Register" element={<Register/>} />
-         
-        <Route path="/Login" element={<Login/>} />
-      </Routes>
-
-
-
-    </Router>
-
-   
-    </div>
- 
-  );
-=======
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import {Login} from "./pages/login";
-import Registeration from "./pages/registeration";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Login/Register";
 import Dashboard from "./admin/Dashboard";
 import ShowUsers from "./admin/showUsers";
- 
+import { createContext, useState } from "react";
+export const userContext = createContext();
 
 function App() {
+
+  const [userData, setUserData] = useState([]);
+
   return (
+    <div className="app">
+        <userContext.Provider value={{ userData, setUserData }}>
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/register" element={<Registeration />} />
+        <Route exact path="/register" element={<Register />} />
         <Route exact path="/Users" element={<ShowUsers/>} />
       </Routes>
     </Router>
+    </userContext.Provider>
+
+    </div>
   )
->>>>>>> 92d89c1d706010298b47208f4d2e80fcf4b3ad3b
 }
  
  

5327647e27817b92066ec62bcbfa800fde8b0d84
Abdullah_assoli <abdullahassoli@gmail.com>
2022-07-31 22:10:34 +0200

    Update Comment.php

--- laravelapi/app/Models/Comment.php
+++ laravelapi/app/Models/Comment.php
@@ -8,9 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    protected $fillable = ['user_id','text'];
-=======
         protected $fillable = ['user_id','text'];
->>>>>>> d8cf9d1227bfaba8d570cfbd270b8811e142cbc8
 }

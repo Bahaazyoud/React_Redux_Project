f8f83cd2cd26d336cc92830bb030b4210427fef7
RubaAlmasri <100359514+RubaAlmasri@users.noreply.github.com>
2022-07-31 22:45:21 +0300

    main

--- laravelapi/app/Http/Controllers/CommentController.php
+++ laravelapi/app/Http/Controllers/CommentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use Illuminate\Http\Request;
-use App\Models\Comment;
 
 
 class CommentController extends Controller
--- redux_project/src/App.js
+++ redux_project/src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Login/Register";
 import Login from "./pages/Login/login";
 import { Home } from "./pages/Home";
-<<<<<<< HEAD
 import Comments from "./admin/Comments";
 function App() {
   return (
@@ -34,23 +33,6 @@ function App() {
     </Router>
 
    
-=======
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          {/* <Route exact path="/register" element={<Registeration />} /> */}
-          <Route exact path="/Users" element={<ShowUsers />} />
-          <Route exact path="/addUser" element={<AddUser />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/Posts" element={<Posts />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </Router>
->>>>>>> d8cf9d1227bfaba8d570cfbd270b8811e142cbc8
     </div>
   );
 };
